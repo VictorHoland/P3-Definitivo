6710d0d33057aac265cf4ca0d8e458c9b4142c00
VictorHoland <54122262+VictorHoland@users.noreply.github.com>
2020-02-12 05:07:20 -0300

    Update FuncaoAux.java

--- FuncaoAux.java
+++ FuncaoAux.java
@@ -1,28 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projeto;
 
 /**
  *
- * @author Beatriz
+ * @author Victor
  */
-public class Faxineiro extends Funcionario{
-    
-    private String setor;
-    
-    public Faxineiro(String pNome, int pIdade, int pSalario, String pCpf, String pFuncao) {
-        super(pNome, pIdade, pSalario, pCpf, pFuncao);
-    }
-
-    public String getSetor() {
-        return setor;
-    }
-
-    public void setSetor(String setor) {
-        this.setor = setor;
+public class FuncaoAux {
+    public boolean cpfValido(String cpf){
+        return !(cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") ||
+                cpf.equals("33333333333")|| cpf.equals("44444444444") || cpf.equals("55555555555") ||
+                cpf.equals("66666666666")|| cpf.equals("77777777777") || cpf.equals("88888888888") ||
+                cpf.equals("99999999999")|| cpf.length() != 11);
     }
     
+    public boolean entradaValida1(int entrada){
+        return entrada >= 1 && entrada <3;
+    }
+    
+    public boolean entradaValida2(int entrada){
+        return entrada >= 1 && entrada <4;
+    }
 }
