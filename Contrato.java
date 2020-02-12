6a10a79b9869806717e79900773fc39fe693ea1a
VictorHoland <54122262+VictorHoland@users.noreply.github.com>
2020-02-12 04:59:22 -0300

    Add files via upload

---
+++ Contrato.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author Victor
+ */
+public interface Contrato {
+    
+    public void bonus();
+    public void puniçao();
+    public boolean getDisponibilidade();
+    public void setDisponibilidade(boolean disponivel);
+}
